2d38cb06f1a4417a2e48094ac6f3bee5d1a1f0e3
LyPf1 <163436671+LyPf1@users.noreply.github.com>
2026-01-09 15:39:49 -0300

    Update lambda_function.py

--- Desenvolvendo Pipeline Real Time/lambda_function.py
+++ Desenvolvendo Pipeline Real Time/lambda_function.py
@@ -3,26 +3,26 @@ import requests
 import boto3
 import os
 
-# Configurações da API
+#API config
 latitude = -29.6846 
 longitude = -51.1419 
 TOMORROW_API_KEY = os.getenv('TOMORROW_API_KEY')
 url = f"https://api.tomorrow.io/v4/weather/realtime?location={latitude},{longitude}&apikey={TOMORROW_API_KEY}"
 headers = {"accept": "application/json"}
 
-# Configurações do Kinesis
+#Kinesis config
 STREAM_NAME = "broker"
 REGION = "us-east-1"
 
-# Cliente do Kinesis
+#customer config
 kinesis_client = boto3.client('kinesis', region_name=REGION)
 
 def lambda_handler(event, context):
-    # Faz a requisição à API
+    #api request
     response = requests.get(url, headers=headers)
     weather_data = response.json()
 
-    # Envia os dados para o Kinesis
+    #send data to Kinesis
     kinesis_client.put_record(
         StreamName=STREAM_NAME,
         Data=json.dumps(weather_data),
@@ -33,3 +33,4 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Dados enviados ao Kinesis com sucesso')
     }
+
